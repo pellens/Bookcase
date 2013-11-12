327bbaa86f24d4e9d1e95c10ca30e1adca9af196
pellens <gert.pellens@gmail.com>
2013-11-13 00:42:21 +0100

    Deleted testfiles

--- application/views/index.php
+++ application/views/index.php
@@ -11,17 +11,6 @@
     	<div class="install-box">
 			<h1>Bookcase freshly installed!</h1>
 			<p><?=anchor("admin","Visit the core backend");?></p>
-			<?
-				$config["twitter_feed_username"] = "gpellens";
-				$config["twitter_feed_limit"] = 10;
-				$config["twitter_feed_view"] = false;
-
-				$this->social->initialize($config);
-
-				echo $this->social->facebook_like("http://facebook.com/journize");
-
-				print_r($this->social->twitter_feed());
-			?>
 		</div>
 	</body>
 </html>
