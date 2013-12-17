eea4e300ab2fb0799aa16c0ab8a3bafe15650775
pellens <gert.pellens@gmail.com>
2013-12-17 13:41:54 +0100

    Module per module update

--- application/views/backend/Translate/translation_overview.php
+++ application/views/backend/Translate/translation_overview.php
@@ -3,8 +3,33 @@
 	<div class="tabs">
 
 		<ul class="links">
-			<li>Translations overview</li>
+			<li class="active">Translations overview</li>
 		</ul>
+
+		<div class="panes">
+
+			<div class="pane active">
+				
+				<table class="table table-bordered table-striped">
+					<tr>
+						<th>Keyword</th>
+						<? foreach($this->translate->all_supported_languages() as $lang):?>
+						<th><?=strtoupper($lang->code);?></th>
+						<? endforeach;?>
+						<th>&nbsp;</th>
+					</tr>
+					<? foreach($list as $trans):?>
+					<tr>
+						<td><?=$trans->key;?></td>
+						<td>Done of niet?</td>
+						<td><?=anchor("admin/lib/translate/edit_translation/".$trans->id,"Edit");?></td>
+					</tr>
+					<? endforeach;?>
+				</table>
+
+			</div>
+
+		</div>
 
 	</div>
 
