36294acecb2a0b3db8b03579290084054f55fbfa
pellens <gert.pellens@gmail.com>
2013-12-25 20:21:56 +0100

    Auto page title

--- application/views/index.php
+++ application/views/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 	<head>
-    	<title>Bookcase</title>
     	<?=$this->core->metatags();?>
     	<link href="<?=base_url("css/backend/backend.css");?>" rel="stylesheet" media="screen">
 
 	</head>
 	<body>
+		<h1><?=$this->core->website_title;?></h1>
 		<?=langswitch();?>
 		<?=crumbs();?>
     	<div class="install-box">
